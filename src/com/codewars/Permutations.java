cc6b0e41cef7743d4289bdd6edb82b7bc12408bc
Thiyagarajan B <thyagubala@gmail.com>
2019-02-22 23:52:57 +0530

    Permutations

---
+++ src/com/codewars/Permutations.java
@@ -0,0 +1,37 @@
+package com.codewars;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+/**
+ * https://www.codewars.com/kata/permutations/
+ */
+class Permutations {
+    public static List<String> singlePermutations(String s) {
+        List<String> result = new ArrayList<>();
+        permute(0, new StringBuilder(s), result);
+        return result
+                .stream()
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    private static void permute(int l, StringBuilder sb, List<String> result) {
+        if (l == sb.length()) {
+            result.add(sb.toString());
+        } else {
+            for (int i = l; i < sb.length(); i++) {
+                swap(sb, i, l);
+                permute(l + 1, new StringBuilder(sb), result);
+                swap(sb, i, l);
+            }
+        }
+    }
+
+    private static void swap(StringBuilder sb, int i, int j) {
+        char t = sb.charAt(i);
+        sb.setCharAt(i, sb.charAt(j));
+        sb.setCharAt(j, t);
+    }
+}
